b32c7b47006c193d41340c175ff866d3a607c43b
doy <doy@tozt.net>
2008-11-26 16:01:22 -0500

    add an output module to help with testing (that just outputs to an internal buffer)

---
+++ t/lib/Log/Dispatch/ToString.pm
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+package Log::Dispatch::ToString;
+use base 'Log::Dispatch::Output';
+
+sub new {
+    my $class = shift;
+    my $self = bless {}, $class;
+    $self->_basic_init(@_);
+    $self->{_string} = '';
+    return $self;
+}
+
+sub log_message {
+    my $self = shift;
+    my %args = @_;
+    $self->{_string} .= $args{message};
+}
+
+sub get_string {
+    my $self = shift;
+    return $self->{_string};
+}
+
+1;
